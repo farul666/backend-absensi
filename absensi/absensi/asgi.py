7393c6837aadad999fb42f72eb303c8c64530e65
farul666 <farul123654@gmail.com>
2023-08-16 11:53:41 +0700

    menambah app dan template

---
+++ absensi/absensi/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for absensi project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'absensi.settings')
+
+application = get_asgi_application()
