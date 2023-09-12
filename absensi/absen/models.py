d44b10b0283c074193a1008856813bd4f9343bdd
Mazaksih <hayat45maki@gmail.com>
2023-09-12 09:57:08 +0700

    Tgl sementara

--- absensi/absen/models.py
+++ absensi/absen/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from biodata.models import Biodata
+from datetime import datetime
 
 # nama, tanggal awal izin & tangggal akhir izin serta keterangan
 
 
 class Absen(models.Model):
     nama=models.ForeignKey(Biodata,on_delete=models.CASCADE,null=True)
-    tgl=models.DateTimeField(auto_now_add=True)
+    tgl=models.DateTimeField(default=datetime.now, blank=True)
     status=models.CharField(max_length=15)
 
 
--- absensi/perizinan/admin.py
+++ absensi/perizinan/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import *
 
 class kolomdata(admin.ModelAdmin):
-    list_display = ['nama']
+    list_display = ['nama','tgl','keterangan']
     search_fields = ['nama']
     list_filter = ('nama',)
     list_per_page = 7
