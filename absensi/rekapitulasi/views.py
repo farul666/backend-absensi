4f957516d579edd185f7015fda9bb182400e1f67
Mazaksih <hayat45maki@gmail.com>
2023-10-12 19:19:28 +0700

    fix rekap

--- absensi/rekapitulasi/views.py
+++ absensi/rekapitulasi/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rekapitulasi.models import Rekap
+from biodata.models import Biodata
 
 def rekap_data(request):
     rekap = Rekap.objects.filter(id=1).first()
